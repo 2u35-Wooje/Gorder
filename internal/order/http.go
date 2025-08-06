10464ab3d7dbbf6882fb89365564528c449a2840
woojejiang <Angel_gap123@163.com>
2025-08-07 02:24:13 +0800

    初始提交

---
+++ internal/order/http.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type HTTPServer struct{}
+
+func (H HTTPServer) PostCustomerCustomerIdOrders(c *gin.Context, customerId string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (H HTTPServer) GetCustomerCustomerIdOrdersOrderId(c *gin.Context, customerId string, orderId string) {
+	//TODO implement me
+	panic("implement me")
+}
--- internal/order/main.go
+++ internal/order/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/2u35-Wooje/gorder-v2/common/config"
+	"github.com/2u35-Wooje/gorder-v2/common/server"
+	"github.com/2u35-Wooje/gorder-v2/order/ports"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"log"
 )
@@ -13,5 +16,13 @@ func init() {
 }
 
 func main() {
-	log.Printf("%v", viper.Get("order"))
+	serviceName := viper.GetString("order.service-name")
+	server.RunHTTPServer(serviceName, func(router *gin.Engine) {
+		ports.RegisterHandlersWithOptions(router, HTTPServer{}, ports.GinServerOptions{
+			BaseURL:      "/api",
+			Middlewares:  nil,
+			ErrorHandler: nil,
+		})
+
+	})
 }
